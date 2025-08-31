48c7b68f383a8fdc4abadd03ec7eb0ba721ead1e
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-08-31 11:54:18 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,22 @@
+public class Unary {
+    public static void main(String[] args){
+        int x=5;
+        int y=-x;
+        int z=-y;
+
+        System.out.println(y);
+        System.out.println(z);
+
+        int a=7;
+        a=a +1;
+       System.out.println(a);
+        a +=1;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        ++a;
+
+        System.out.println(a);
+
+    }
+}
