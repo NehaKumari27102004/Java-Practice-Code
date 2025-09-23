f07ccbe1eafedde2553c760ce0ba24cdabb55123
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-09-23 22:02:53 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,21 @@
+package Object;
+
+public class Driver {
+    public static void main(String[] args) {
+     //   Car myCar=new Car();
+//        myCar.addFuel(6);
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.drive();
+//        myCar.addFuel(3);
+//        myCar.drive();
+//        System.out.println(myCar.getCurrentFuelLevel());
+
+        Car Swift=new Car();
+        //Swift.addFuel(6);
+        //Swift.Start();
+        //Swift.drive();
+        Swift.Start().drive();
+        System.out.println(Swift.color)
+    }
+}
