4d43866f171800bcd1ad0f86a5497a6dd098baa3
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-09-28 09:04:55 +0530

    Add files via upload

---
+++ InitialisationOfArrays.java
@@ -0,0 +1,9 @@
+public class InitialisationOfArrays {
+
+    public static void main(String [] args){
+        int[] arr={76,88,13,82,83};
+        for(int i=0; i<=3; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
