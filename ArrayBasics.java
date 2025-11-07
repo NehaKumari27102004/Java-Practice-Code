863a9fbf0c7004493e573ee0c7cd7d9f7975c2f1
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-07 18:36:54 +0530

    Add files via upload

---
+++ ArrayBasics.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args){
+        int[] x={2,4,5,6,7,22,68,99};
+        System.out.println(Arrays.toString(x));
+    }
+}
