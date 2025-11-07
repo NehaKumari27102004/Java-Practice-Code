f5dd251c18a9c1b95bce19395b171667c0fff5a8
Neha <161303798+NehaKumari27102004@users.noreply.github.com>
2025-11-07 18:37:17 +0530

    Add files via upload

---
+++ OutputInputArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.*;
+public class OutputInputArray {
+    public static void main(String[] args){
+       /* int[] arr={5,-7,9,3,99,55};
+        int n=arr.length;
+        for(int i=0; i<n; i++ ){
+            System.out.print(arr[i]+" ");
+        }
+
+        */
+
+        /*int[] karan=new int[7];
+        for(int i=0; i<7; i++){
+            System.out.println(karan[i]+ " ");
+        }
+
+         */
+        
+    }
+
+}
